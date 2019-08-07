6cc12a8d8023068b837a2cf7be7ef5fbe9fc246a
gayu2723 <53179976+gayu2723@users.noreply.github.com>
2019-08-07 21:40:25 +0530

    Create oddoddinterval.py

---
+++ oddoddinterval.py
@@ -0,0 +1,10 @@
+def oddinterval():
+    n=int(input())
+    m=int(input())
+    for num in range(n,m+1):
+        if(num%2!=0):
+            print(num,end=" ")
+oddinterval()
+            
+    
+        
