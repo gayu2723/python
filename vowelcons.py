f3fe1de948d3cefd024ed3e6cac4f1069037413c
gayu2723 <53179976+gayu2723@users.noreply.github.com>
2019-08-10 13:40:11 +0530

    Create vowelcons.py

---
+++ vowelcons.py
@@ -0,0 +1,7 @@
+let=input()
+if let=='a'or let=='A' or let=='e' or let=='E' or let=='i' or let=='I' or let=='O' or n=='o' or n=='u' or n=='U':
+   print("Vowel")
+elif let>='a'and let<='z' or let>='A' and let<='Z':
+   print("Consonant")   
+else:
+  print("Invalid")   
