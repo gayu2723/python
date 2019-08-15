48e558479596e238f692f67a6c259ee8688ebee8
gayu2723 <53179976+gayu2723@users.noreply.github.com>
2019-08-15 18:16:14 +0530

    Update P47interiortri.py

--- P47interiortri.py
+++ P47interiortri.py
@@ -1,7 +1,9 @@
 
+
 a,b,c=list(map(int,input().split()))
-if(a==b and c<=180):
+if(a==b and a==c and b==c and c<=180 and b==a and c==b and c==a):
     print('yes')
 else:
     print('no')
         
+
