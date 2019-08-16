61b95e554b68c982cffebe641142c23e41bb9071
gayu2723 <53179976+gayu2723@users.noreply.github.com>
2019-08-16 20:44:10 +0530

    Update and rename vowel.py to B3vowel.py

---
+++ B3vowel.py
@@ -0,0 +1,7 @@
+let=input()
+if (let=='a'or let=='A' or let=='e' or let=='E' or let=='i' or let=='I' or let=='O' or let=='o' or let=='u' or let=='U'):
+   print("Vowel")
+elif (let>='a'and let<='z' or let>='A' and let<='Z'):
+   print("Consonant")   
+else:
+  print("Invalid")   
--- vowel.py
+++
@@ -1,7 +0,0 @@
-n=input()
-if n=='a'or n=='A' or n=='e' or n=='E' or n=='i' or n=='I' or n=='O' or n=='o' or n=='u' or n=='U':
-   print("Vowel")
-elif n>='a'and n<='z' or n>='A' and n<='Z':
-   print("Consonant")   
-else:
-  print("Invalid")   
