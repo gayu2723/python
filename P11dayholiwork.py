154b53c68b6e7e53f8a1ae4d2286fbf9ad2c8e90
gayu2723 <53179976+gayu2723@users.noreply.github.com>
2019-08-17 16:13:48 +0530

    Create P11dayholiwork.py

---
+++ P11dayholiwork.py
@@ -0,0 +1,8 @@
+day=input()
+if(day=="Sunday" or day=="Suturday"):
+    print('yes')
+
+elif(day=="Monday" or day=="Tuesday" or day=="Wednesday" or day=="Thursday" or day=="Friday"):
+    print('no')
+else:
+    print('invalid')
