4148a109af7b514b53d2333b573d6bca5338b274
gayu2723 <53179976+gayu2723@users.noreply.github.com>
2019-08-24 17:40:29 +0530

    Update BitAND.py

--- BitAND.py
+++ BitAND.py
@@ -1,5 +1,10 @@
 a=int(input())
-b,c=list(map(int,input().split()))[:a]
-if(a<=100000):
-  mul=b&c
-  print(mul)
+c=list(map(int,input().split()))[:a]
+for i in range(0,a):
+    if(a<=100000):
+        mul=c[i]&c[i+1]
+        print(mul)
+        break
+    else:
+        print(invalid)
+
